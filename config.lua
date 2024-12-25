4ad6c059bf8a911d95d51dae5fa0bf979dbb275a
Shinaii <contact@shinaii.xyz>
2024-12-25 23:40:13 +0100

    first commit

---
+++ config.lua
@@ -0,0 +1,15 @@
+return {    
+    tag_refresh = {
+        newgame = true,
+        enable = true,
+        refresh_key = "f1",
+        refresh_gamepad = "rightshoulder",
+        path = "Collection/Gameplay/TagRefresh.lua",
+    },
+    reroll_shop = {
+        enable = true,
+        voucher = false,
+        booster = true,
+        path = "Collection/Gameplay/RerollShop.lua",
+    },
+}
---
+++ localization/en-us.lua
@@ -0,0 +1,30 @@
+return {
+    misc = {
+        dictionary = {
+            -- All tabs
+            tabs_gameplay = "Gameplay",
+            tabs_fixes = "Fixes",
+            tabs_func = "Functionality",
+            tabs_help = "Help",
+
+            -- Fixes tab
+
+            -- Functionality tab
+
+            -- Gameplay tab
+
+            gameplay_tag_refresh = "Tag Refresh",
+            gameplay_tag_refresh_desc = "Mod Desc: Reroll tags simple and easy!",
+            gameplay_tag_refresh_enable = "Should this mod be enabled?",
+            gameplay_tag_refresh_newgame = "Only Reroll Tags on New Game?",
+            gameplay_reroll_shop = "Reroll Shop",
+            gameplay_reroll_shop_desc = "Mod Desc: Rerolls the whole shop!",
+            gameplay_reroll_shop_enable = "Should this mod be enabled?",
+            gameplay_reroll_shop_booster = "Reroll Booster?",
+            gameplay_reroll_shop_voucher = "Reroll Voucher?",
+
+            -- Help tab
+            help = [[If you need help, please contact me on Discord or GitHub!]],
+        },
+    }
+}
